6897709d3290d935c8eb0128480e9daa36981e65
BitCircuit <sam_feng@icloud.com>
2022-07-20 04:10:11 +0100

    Added JSON

---
+++ PointCloud2LookingGlass/src/jsonHandler/jsonHandler.cpp
@@ -0,0 +1,35 @@
+#include "jsonHandler.h"
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/reader.h"
+#include <rapidjson/ostreamwrapper.h>
+#include <rapidjson/istreamwrapper.h>
+#include "fstream"
+
+#include "iostream"
+
+using namespace rapidjson;
+using namespace std;
+
+void readJSON() {
+    ifstream ifs("output.json");
+    IStreamWrapper isw(ifs);
+
+    Document d;
+    d.ParseStream(isw);
+}
+
+void newJSON(char* destPath) {
+    const char json[] = " { \"hello\" : \"world\", \"t\" : true , \"f\" : false, \"n\": null, \"i\":123, \"pi\": 3.1416, \"a\":[1, 2, 3, 4] } ";
+
+    Document d;
+    d.Parse(json);
+
+    ofstream ofs(destPath);
+    OStreamWrapper osw(ofs);
+
+    Writer<OStreamWrapper> writer(osw);
+    d.Accept(writer);
+}
---
+++ PointCloud2LookingGlass/src/jsonHandler/jsonHandler.h
@@ -0,0 +1,64 @@
+#pragma once
+#define _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING
+void newJSON(char*);
+void readJSON();
+
+/*
+Path: same as executable file
+profileCamera.json
+{
+	// Online means the file is downloaded from online, no need to set default values
+	"knownModels" : ["Kinect", "RealSense", "Record3D", "Heges", "Online"], 
+	"KinectProfile" : "profileKinect.json"			// act as path
+	// ... same for other models
+}
+
+profileKinect.json
+{
+	"cameraPosition" : [1.0, 0.0, 0.0],
+	"focalPoint" : [0.0, 0.0, 0.0],
+	"viewUp" : [0.0, 0.0, 1.0], 
+	"clippingRange" : [0.1, 0.1]
+}
+*/
+
+/*
+Path: same as bunch of original PLY
+videoConfig.json
+{
+	"cameraModel" : "Kinect", 
+	"overrideCameraPosition" : [1.0, 0.0, 0.0],		// Not included in new JSON, only added if camera changed
+	"overrideFocalPoint" : [0.0, 0.0, 0.0],			// Not included in new JSON, only added if camera changed
+	"overrideViewUp" : [0.0, 0.0, 1.0],				// Not included in new JSON, only added if camera changed
+	"overrideClippingRange" : [0.1, 0.1],			// Not included in new JSON, only added if camera changed
+	"FPS" : 30,
+	"hasColor" : true,
+	"isSrcHasFaceEmbed" : false,		// update 
+	"meshedPLYPath" : "path"			// if isSrcHasFaceEmbed == false
+}
+*/
+
+/*
+For unrelated PLY files:
+Name: same as ply file
+For ply downloaded from online:
+{
+	"cameraModel" : "Online", 
+	"overrideCameraPosition" : [1.0, 0.0, 0.0],		// Not included in new JSON, only added if camera changed
+	"overrideFocalPoint" : [0.0, 0.0, 0.0],			// Not included in new JSON, only added if camera changed
+	"overrideViewUp" : [0.0, 0.0, 1.0],				// Not included in new JSON, only added if camera changed
+	"overrideClippingRange" : [0.1, 0.1],			// Not included in new JSON, only added if camera changed
+}
+
+For ply get from camera:
+{
+	"cameraModel" : "Kinect", 
+	"overrideCameraPosition" : [1.0, 0.0, 0.0],		// Not included in new JSON, only added if camera changed
+	"overrideFocalPoint" : [0.0, 0.0, 0.0],			// Not included in new JSON, only added if camera changed
+	"overrideViewUp" : [0.0, 0.0, 1.0],				// Not included in new JSON, only added if camera changed
+	"overrideClippingRange" : [0.1, 0.1],			// Not included in new JSON, only added if camera changed
+	"hasColor" : true,
+	"isSrcHasFaceEmbed" : false,		// update
+	"meshedPLYPath" : "path"			// if isSrcHasFaceEmbed == false
+}
+*/
