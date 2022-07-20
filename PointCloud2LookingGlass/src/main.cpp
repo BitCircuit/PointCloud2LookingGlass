946da18dfa157f682825589e654871a713e2792b
BitCircuit <sam_feng@icloud.com>
2022-07-20 04:11:08 +0100

    Backup

--- PointCloud2LookingGlass/src/main.cpp
+++ PointCloud2LookingGlass/src/main.cpp
@@ -1,5 +1,6 @@
 //#include "readPointCloudFile/plyReader.h"
 #include "vtkHandler/vtkHandler.h"
+#include "jsonHandler/jsonHandler.h"
 
 #include "stdio.h"
 #include "String.h"
@@ -17,6 +18,7 @@ int main(int argc, char* argv[]) {
     else {
         if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) usage();
         else if (strcmp(argv[1], "-i") == 0 || strcmp(argv[1], "--input") == 0) vtkHandler(argv);
+        else if (strcmp(argv[1], "-j") == 0) newJSON(argv[1]);
         else    usage();
     }
     return 0;
